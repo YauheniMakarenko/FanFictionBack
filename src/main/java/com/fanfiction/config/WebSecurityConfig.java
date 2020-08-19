1f010410c7556dce75913676eb1ab36a7f79790c
Yauheni Makarenka <loxix_1996@mail.ru>
2020-08-19 16:59:35 +0300

    Commit END 6.1

--- src/main/java/com/fanfiction/config/WebSecurityConfig.java
+++ src/main/java/com/fanfiction/config/WebSecurityConfig.java
@@ -37,7 +37,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter implements W
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOrigins("http://localhost:4200")
+                        .allowedOrigins("https://fanfictionfront.herokuapp.com/")
                         .allowedMethods("*");
             }
         };
